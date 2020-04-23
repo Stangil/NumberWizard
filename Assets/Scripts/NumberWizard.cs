2277ca670631088198800c37e8b151e5ccb3b364
Stangil <stangil44@gmail.com>
2020-04-23 16:09:30 -0400

    Setting up layout on core game page

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] int guess;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    // Use this for initialization
+    void Start()
+    {
+        guessText = GetComponent<TextMeshProUGUI>() ?? gameObject.AddComponent<TextMeshProUGUI>();
+        guessText.text = guess.ToString();
+    }
+
+    public void ToLow()
+    {
+        Debug.Log("ToLow");
+        min = guess;
+        NextGuess();
+    }
+
+    public void ToHigh()
+    {
+        Debug.Log("ToHigh");
+        max = guess;
+        NextGuess();
+    }
+     
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log(guess);
+        guessText.text = guess.ToString();
+    }
+}
